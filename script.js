6e6a8274d4bddfe67e744cb133970f1ab7e74290
Baga Kenny <kennybagajunior@gmail.com>
2022-08-02 23:02:24 +0200

    Responsive page coded

---
+++ script.js
@@ -0,0 +1,11 @@
+const tl = new gsap.timeline();
+
+tl.from('.img, .heading, .menu, h1, p, .bottom-section', {
+    duration: 1,
+    y: 200,
+    opacity: 0,
+    stagger: {
+        amount: 0.5,
+    },
+
+});
